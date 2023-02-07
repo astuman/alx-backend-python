4f74dbdb19d44e284fc08378a0317281db54d3b8
Astewul <loloalex1992@gmail.com>
2023-02-07 16:46:58 +0300

    update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,6 @@ import _asyncio
 
 async def async_generator() -> Generator[float, None, None]:
     """Loop 10 times asynchronously"""
-
     for i in range(10):
         yield random(0,10)
         await _asyncio.sleep(1)
